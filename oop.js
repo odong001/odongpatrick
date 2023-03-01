a963af102aa474640e4f3d166acf56e79d77f98b
odong001 <111044828+odong001@users.noreply.github.com>
2023-03-01 12:19:09 +0300

    Add files via upload

---
+++ oop.js
@@ -0,0 +1,180 @@
+// OOP
+// Object-Oriented Programming(OOP) is a programming paradigm based on the concepts of Objects.
+// That means everything that we achieve in OOP language is through objects here object defines the real-world entities like Student or Car (More detail soon).
+// By using OOP, you make sure that only allowed members of one code is accessible to others. That makes your code fully secured to unauthenticated access (within the code).
+// example.
+
+class User {
+    #password;
+    constructor(name, userName, password ,ninNumber ,phoneNumber) {
+      this.name = name;
+      this.userName = userName;
+      this.#password = password;
+      this.ninNumber = ninNumber;
+      this.phoneNumber = phoneNumber;
+    }
+}
+let login = ("name","userName","password","ninNumber","phoneNumber"
+)
+console.log(user_login)
+
+// example 2
+
+class Dog {
+  constructor(name, breed, color, age,size){
+    this.name = name;
+    this.breed= breed;
+    this.color = color;
+    this.age = age;
+    this.size = size;
+  }
+}
+
+let Dog=("jesse","german","black","10","size")
+
+console.log(Dog)
+
+// example 3
+
+class Book{
+
+  constructor(title,author,numberOfpage,publisher,size){
+    this.title = title;
+    this.author = author;
+    this.numberOfpage = numberOfpage;
+    this.publisher = publisher;
+    this.size = size;
+  }
+}
+
+let Book=("Javascrip","irene","100","refactory","big")
+
+console.log("printing out this key ward ",Book)
+
+
+// example 4
+
+class Phone{
+  constructor(storage,bettery,screensize,sizs,ram,){
+    this.storage = storage;
+    this.bettery = bettery;
+    this.screensize = screensize;
+    this.size = size;
+    this.ram = ram;
+
+  }
+}
+
+let Book=("128","strong","100","6","small")
+
+console.log("printing out this key ward ",Phone)
+
+// example 5
+
+class Form{
+  constructor(phoneNumber,User,userName,recoveryNumber, password){
+    this.phoneNumber = phoneNumber;
+    this.user = user;
+    this.userName = userName;
+    this.recoveryNumber = recoveryNumber;
+    this.password = password;
+
+
+  }
+}
+
+let Form=("phoneNumber","user","userName","recoveryNumber","passward")
+
+console.log("printing out this key ward" ,Form)
+
+
+// example 6
+
+class Student{
+  constructor(name,height,age,weight, color){
+    this.name = name;
+    this.height = height;
+    this.age = age;
+    this.weight = weight;
+    this.color = color;
+ 
+
+  }
+}
+
+let Form=("name","height","age","weight","color")
+
+console.log("printing out this key ward" ,Student)
+
+
+// example7
+class Chair{
+  constructor(plastic,roko,sofa,lsharp,matelic){
+      this.plastic = plastic;
+    this.roko = roko;
+    this.sofa = roko;
+    this.lsharp = lsharp;
+    this.matelic = matelic;
+ 
+
+  }
+}
+
+let Form=("plastic","roko","sofa","lsharp","matelic")
+
+console.log("printing out this key ward" ,Chair)
+
+// examle8
+
+class Computer{
+  constructor(size,color,type,sharp,capacity){
+    this.size = size;
+    this.color = color;
+    this.type = type;
+    this.sharp = sharp;
+    this.capacity = capacity;
+ 
+
+  }
+}
+
+let Form=("14.5","black","hp","sharp","cpacity")
+
+console.log("printing out this key ward" ,Computer)
+
+// example 9
+
+class motorcyle{
+  constructor(baja,type,color,tvs,speed){
+    this.baja = baja;
+    this.color = color;
+    this.type = type;
+    this.tvs = tvs;
+    this.speed = speed;
+ 
+
+  }
+}
+
+let Form=("baja","black","type","tvs","speed")
+
+console.log("printing out this key ward" ,motorcyle)
+
+// example 10
+
+class Tribe{
+  constructor(color,weight,height,region,behavior){
+    this.weight = weight;
+    this.color = color;
+    this.height = height;
+    this.region = region;
+    this.behavior = behavior;
+ 
+
+  }
+}
+
+let Form=("weight","black","tall","region","behavior")
+
+console.log("printing out this key ward" ,Tribe)
+
